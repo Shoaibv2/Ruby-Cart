d8c2a8fbbe8a7c419685a20140ab9b553536a56e
Shoaibv2 <skrj1337@gmail.com>
2022-12-29 23:44:23 +0500

    Add Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,78 @@
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <footer className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+          <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+            <Link href={"/"} className="flex md:justify-start justify-center ">
+              <Image src="/logo.png" width={210} height={100} />
+            </Link>
+          </div>
+          <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+            <div className="lg:w-1/3 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-1">CATEGORIES</h2>
+              <nav className="list-none mb-10">
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">Men</a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">Women</a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">Teen</a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">Kids</a>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-1/3 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-1">Customer Care</h2>
+              <nav className="list-none mb-10">
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">Help Center</a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">Returns & Refunds</a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">Contact Us</a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">Purchase Protection</a>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-1/3 md:w-1/2 w-full px-4">
+
+              <nav className="list-none mb-10 mt-5">
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">About Us</a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">Terms & Conditions</a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">Privacy Policy</a>
+                </li>
+
+              </nav>
+            </div>
+
+          </div>
+        </div>
+        <div className="bg-gray-100">
+          <div className="container mx-auto py-2 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p className="text-gray-500 text-sm text-center sm:text-left">© 2023 RubyCart —
+              <a href="https://twitter.com/shoaibkhanv2" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@shoaibkhanv2</a>
+            </p>
+
+          </div>
+        </div>
+      </footer>
+    </div>
   )
 }
