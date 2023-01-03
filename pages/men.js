08c6a83dc4177fe45dac207f8e29c20d1ffd44a5
Shoaibv2 <skrj1337@gmail.com>
2023-01-04 01:06:29 +0500

    Add Products

--- pages/men.js
+++ pages/men.js
@@ -1,8 +1,101 @@
 
 function Men() {
   return (
-    <div>Men</div>
-  )
+    <section>
+      <div className="container py-16 px-4 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
+            <a href="#">
+              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men-2.jpg" alt="" className="object-cover object-center" />
+              </div>
+            </a>
+            <a href="" className="mt-3 block">
+              <span className="pl-[2px] text-sm font-medium text-gray-900">Trendy T-Shirt</span>
+              <p className="mt-1 text-sm text-gray-600 pl-[2px]">Shop Now</p>
+            </a>
+          </div>
+          <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
+            <a href="#">
+              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men-2.jpg" alt="" className="object-cover object-center" />
+              </div>
+            </a>
+            <a href="" className="mt-3 block">
+              <span className="pl-[2px] text-sm font-medium text-gray-900">Trendy T-Shirt</span>
+              <p className="mt-1 text-sm text-gray-600 pl-[2px]">Shop Now</p>
+            </a>
+          </div>
+          <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
+            <a href="#">
+              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men-2.jpg" alt="" className="object-cover object-center" />
+              </div>
+            </a>
+            <a href="" className="mt-3 block">
+              <span className="pl-[2px] text-sm font-medium text-gray-900">Trendy T-Shirt</span>
+              <p className="mt-1 text-sm text-gray-600 pl-[2px]">Shop Now</p>
+            </a>
+          </div>
+          <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
+            <a href="#">
+              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men-2.jpg" alt="" className="object-cover object-center" />
+              </div>
+            </a>
+            <a href="" className="mt-3 block">
+              <span className="pl-[2px] text-sm font-medium text-gray-900">Trendy T-Shirt</span>
+              <p className="mt-1 text-sm text-gray-600 pl-[2px]">Shop Now</p>
+            </a>
+          </div>
+          <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
+            <a href="#">
+              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men.jpg" alt="" className="object-cover object-center" />
+              </div>
+            </a>
+            <a href="" className="mt-3 block">
+              <span className="pl-[2px] text-sm font-medium text-gray-900">Trendy T-Shirt</span>
+              <p className="mt-1 text-sm text-gray-600 pl-[2px]">Shop Now</p>
+            </a>
+          </div>
+          <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
+            <a href="#">
+              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men.jpg" alt="" className="object-cover object-center" />
+              </div>
+            </a>
+            <a href="" className="mt-3 block">
+              <span className="pl-[2px] text-sm font-medium text-gray-900">Trendy T-Shirt</span>
+              <p className="mt-1 text-sm text-gray-600 pl-[2px]">Shop Now</p>
+            </a>
+          </div>
+          <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
+            <a href="#">
+              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men.jpg" alt="" className="object-cover object-center" />
+              </div>
+            </a>
+            <a href="" className="mt-3 block">
+              <span className="pl-[2px] text-sm font-medium text-gray-900">Trendy T-Shirt</span>
+              <p className="mt-1 text-sm text-gray-600 pl-[2px]">Shop Now</p>
+            </a>
+          </div>
+          <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
+            <a href="#">
+              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men.jpg" alt="" className="object-cover object-center" />
+              </div>
+            </a>
+            <a href="" className="mt-3 block">
+              <span className="pl-[2px] text-sm font-medium text-gray-900">Trendy T-Shirt</span>
+              <p className="mt-1 text-sm text-gray-600 pl-[2px]">Shop Now</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Men
