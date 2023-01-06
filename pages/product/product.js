39be5b805e9f3290aa78f7ba62ccd52fdaa301f1
Shoaibv2 <skrj1337@gmail.com>
2023-01-06 06:46:51 +0500

    Add Breadcrumb

---
+++ pages/product/product.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div className='container mx-auto py-8'>
+      <div className="flex items-center">
+        <a href="#" className='mr-2 text-base font-medium text-gray-900'>Men <span className='h-5 w-4 text-gray-300'>\</span> </a>
+        <a href="#" className='font-medium text-gray-500 hover:text-gray-600'>Basic T Shirt</a>
+      </div>
+    </div>
+  )
+}
+
+export default Product
