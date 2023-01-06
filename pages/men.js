2879df09901a9de4f65c9d7f9f9cc892d85732cc
Shoaibv2 <skrj1337@gmail.com>
2023-01-06 21:59:04 +0500

    Add Height & Width to Images

--- pages/men.js
+++ pages/men.js
@@ -7,7 +7,18 @@ function Men() {
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
               <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men-2.jpg" alt="" className="object-cover object-center" />
+                <img src="/shirt-men-3.jpg" alt="" className="object-cover object-center h-[288px] w-[288px]" />
+              </div>
+            </a>
+            <a href="" className="mt-3 block">
+              <span className="pl-[2px] text-sm font-medium text-gray-900">Basic T-Shirt</span>
+              <p className="mt-1 text-sm text-gray-600 pl-[2px]">Shop Now</p>
+            </a>
+          </div>
+          <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
+            <a href="#">
+              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men-2.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -18,7 +29,7 @@ function Men() {
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
               <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men-2.jpg" alt="" className="object-cover object-center" />
+                <img src="/t-shirt-men-2.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -29,7 +40,7 @@ function Men() {
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
               <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men-2.jpg" alt="" className="object-cover object-center" />
+                <img src="/t-shirt-men-2.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -40,7 +51,7 @@ function Men() {
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
               <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men-2.jpg" alt="" className="object-cover object-center" />
+                <img src="/t-shirt-men.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -51,7 +62,7 @@ function Men() {
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
               <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men.jpg" alt="" className="object-cover object-center" />
+                <img src="/t-shirt-men.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -62,7 +73,7 @@ function Men() {
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
               <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men.jpg" alt="" className="object-cover object-center" />
+                <img src="/t-shirt-men.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -73,18 +84,7 @@ function Men() {
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
               <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men.jpg" alt="" className="object-cover object-center" />
-              </div>
-            </a>
-            <a href="" className="mt-3 block">
-              <span className="pl-[2px] text-sm font-medium text-gray-900">Trendy T-Shirt</span>
-              <p className="mt-1 text-sm text-gray-600 pl-[2px]">Shop Now</p>
-            </a>
-          </div>
-          <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
-            <a href="#">
-              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men.jpg" alt="" className="object-cover object-center" />
+                <img src="/t-shirt-men.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
