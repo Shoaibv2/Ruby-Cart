482db6f7e67139a305ee3aeef8f3dd6966994f25
Shoaibv2 <skrj1337@gmail.com>
2023-01-08 00:47:10 +0500

    Fix Responsive Issue

--- pages/men.js
+++ pages/men.js
@@ -6,8 +6,8 @@ function Men() {
         <div className="flex flex-wrap -m-4">
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
-              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/shirt-men-3.jpg" alt="" className="object-cover object-center h-[288px] w-[288px]" />
+              <div href="" className=" h-[288px] w-[288px] overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/shirt-men-3.jpg" alt="" className="object-cover object-center w-full h-full" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -17,8 +17,8 @@ function Men() {
           </div>
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
-              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men-2.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
+              <div href="" className="h-[288px] w-[288px] overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men-2.jpg" alt="" className="w-full h-full object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -28,8 +28,8 @@ function Men() {
           </div>
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
-              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/shirt-men-5.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
+              <div href="" className=" h-[288px] w-[288px] overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/shirt-men-5.jpg" alt="" className="w-full h-full object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -39,8 +39,8 @@ function Men() {
           </div>
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
-              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/shirt-men-4.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
+              <div href="" className=" h-[288px] w-[288px] overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/shirt-men-4.jpg" alt="" className="w-full h-full object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -50,8 +50,8 @@ function Men() {
           </div>
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
-              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
+              <div href="" className=" h-[288px] w-[288px] overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men.jpg" alt="" className="w-full h-full object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -61,8 +61,8 @@ function Men() {
           </div>
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
-              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
+              <div href="" className=" h-[288px] w-[288px] overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men.jpg" alt="" className="w-full h-full object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -72,8 +72,8 @@ function Men() {
           </div>
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
-              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
+              <div href="" className=" h-[288px] w-[288px] overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men.jpg" alt="" className="w-full h-full object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
@@ -83,8 +83,8 @@ function Men() {
           </div>
           <div className="lg:w-1/4 p-4 md:w-1/2 w-full group mt-2">
             <a href="#">
-              <div href="" className=" overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
-                <img src="/t-shirt-men.jpg" alt="" className="h-[288px] w-[288px] object-cover object-center" />
+              <div href="" className=" h-[288px] w-[288px] overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                <img src="/t-shirt-men.jpg" alt="" className="w-full h-full object-cover object-center" />
               </div>
             </a>
             <a href="" className="mt-3 block">
